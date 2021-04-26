fb87f85e2bc935ce8842bc776252764e452713ef
Zhur-Zhur <vlad.zhurov@gmail.com>
2021-04-26 12:50:40 -0400

    Fixed timers, added recursive fibonacci alternatives

--- Items/fibonacci/fib.cpp
+++ Items/fibonacci/fib.cpp
@@ -1,55 +1,45 @@
 #include <iostream>
-#include <ctime>
+#include <chrono>
 
-/* Calculate fibonacci numbers recursively using a memo to speed up calculations
- * The attempt to use recursive lambda functions because I wasn't feeling like 
- * passing a refrence to a array to act as a memo has reminded me not to bodge
- * a midnight, it doesn't end well.
- */
-/*
+//Calculate fibonacci numbers recusively without a memo.
+long long fib_terrible(int n){
+	if (n == 0) return 0ll;
+	if (n == 1) return 1ll;
+	return fib_terrible(n-1) + fib_terrible(n-2);
+}
+
+//Calculate fibonacci numbers recursively using a memo to speed up calculations, using lamda functions.
 #include <functional>
-
-long fib_r(int n){
-	if (n < 1) return 0L;
-	if (n == 1) return 1L;
-	 long memo[n];
-	for(int i = 0; i < n; i++) memo[i] = -1L;
+long long fib_r_lamda(int n){
+	//Create memo array in a higher scope.
+	long long memo[n+1];
+	for(int i = 0; i <= n; i++) memo[i] = 0ll;
 	
-	std::function< long(int)> calc_fib = [&](int m){
-		std::cout << m << std::endl;
-		if (memo[m] != -1L){
-			std::cout <<"e1 "<< m << std::endl;
-			std::cout <<"e1 "<< memo[m] << std::endl;
-			return memo[m];
-		}
-		if (m == 0){
-			std::cout <<"e2 "<< m << std::endl;
-			return 0L;
-		}
-		if (m == 1){
-			std::cout <<"e3 "<< m << std::endl;
-			return 1L;
-		}
+	//Lamda function to calculate fibonacci numbers recusively
+	std::function<long long(int)> calc_fib = [&](int m){
+		if (memo[m] != 0ll)	return memo[m];
+		if (m == 0) return 0ll;
+		if (m == 1) return 1ll;
 		memo[m] = calc_fib(m-1) + calc_fib(m-2);
 		return memo[m];
 	};
-	
-	long long calc_fib(int n){
-		if (memo[n] != -1) return memo[n];
-		if (n == 0) return 0;
-		if (n == 1) return 1;
-		memo[n] = calc_fib(n-1) + calc_fib(n-2);
-		return memo[n];
-	}
-	long v = calc_fib(n);
-	std::cout << "-----" << std::endl;
-	for( long num: memo){
-		std::cout << num << " ";
-	}
-	std::cout<<std::endl;
-	return v;
+
+	return calc_fib(n);
 }
-*/
+
+//Calculate fibonacci numbers recursively using a memo to speed up calculations, array instead of dictionary (map).
+long long fib_r_alt(int n, long long *memo = NULL){
+	//Allocate array initialy
+	if (memo == NULL){
+		memo = new long long[n+1];
+		for(int i = 0; i <= n; i++) memo[i] = 0ll;
+	}
+	if (memo[n] != 0ll) return memo[n];
+	if (n == 0) return 0ll;
+	if (n == 1) return 1ll;
+	memo[n] = fib_r_alt(n-1, memo) + fib_r_alt(n-2, memo);
+	return memo[n];
+}
 
 //Calculate fibonacci numbers recursively using a memo to speed up calculations, dictionaries (maps) are nice.
 #include <unordered_map>
@@ -66,9 +56,9 @@ long long fib_i(int n){
 	long long sum_0 = 0,
 		sum_1 = 1,
 		sum_f = 0;
-	if(n < 1) return 0;
-	if(n == 1) return 1;
-	for(int i = 2; i <= n; i++){
+	if (n < 1) return 0;
+	if (n == 1) return 1;
+	for (int i = 2; i <= n; i++){
 		sum_f = sum_0 + sum_1;
 		sum_0 = sum_1;
 		sum_1 = sum_f;
@@ -77,77 +67,59 @@ long long fib_i(int n){
 }
 
 int main(){
-	std::clock_t begin;
-	
-	//Recursive fibonacci .
-	std::cout << "Recursive  fibonacci" << std::endl;
 	//fib(0) = 0
-	begin = clock();
-	std::cout << "fib_r(0) = " << fib_r(0) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
 	//fib(1) = 1
-	begin = clock();
-	std::cout << "fib_r(1) = " << fib_r(1) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
 	//fib(2) = 1
-	begin = clock();
-	std::cout << "fib_r(2) = " << fib_r(2) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
 	//fib(3) = 2
-	begin = clock();
-	std::cout << "fib_r(3) = " << fib_r(3) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
 	//fib(5) = 5
-	begin = clock();
-	std::cout << "fib_r(5) = " << fib_r(5) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
 	//fib(10) = 55
-	begin = clock();
-	std::cout << "fib_r(10) = " << fib_r(10) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
 	//fib(20) = 6765
-	begin = clock();
-	std::cout << "fib_r(20) = " << fib_r(20) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
+	//fib(30) = 832040
+	//fib(40) = 102334155
 	//fib(50) = 12586269025
-	begin = clock();
-	std::cout << "fib_r(50) = " << fib_r(50) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
 
+	std::array<int, 10> test_items =  {0, 1, 2, 3, 5, 10, 20, 30, 40, 50};
+	auto begin = std::chrono::high_resolution_clock::now();
+	//Recursive fibonacci O(n) time, O(n) space. Map memo
+	std::cout << "Recursive  fibonacci (Map Memo)" << std::endl;
+	for(int i : test_items){
+		begin = std::chrono::high_resolution_clock::now();
+		std::cout << "fib_r(" << i << ") = " << fib_r(i) << " ";
+		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - begin).count() << std::endl;
+	}
+
+	//Recursive fibonacci O(n) time, O(n) space. Lamda Recursion, Array Memo
+	std::cout << "\nRecursive  fibonacci (Lamda, Array Memo)" << std::endl;
+	for(int i : test_items){
+		begin = std::chrono::high_resolution_clock::now();
+		std::cout << "fib_r_lamda(" << i << ") = " << fib_r_lamda(i) << " ";
+		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - begin).count() << std::endl;
+	}
+	
+	//Recursive fibonacci O(n) time, O(n) space. Array Memo
+	std::cout << "\nRecursive  fibonacci (Array Memo)" << std::endl;
+	for(int i : test_items){
+		begin = std::chrono::high_resolution_clock::now();
+		std::cout << "fib_r_alt(" << i << ") = " << fib_r_alt(i) << " ";
+		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - begin).count() << std::endl;
+	}
+	
+	//Recursive fibonacci O(2^n) time, O(n) space.
+	std::cout << "\nRecursive  fibonacci (No Memo)" << std::endl;
+	for(int i = 0; i < test_items.size()-1; i++){
+		begin = std::chrono::high_resolution_clock::now();
+		std::cout << "fib_terrible(" << test_items[i] << ") = " << fib_terrible(test_items[i]) << " ";
+		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - begin).count() << std::endl;
+	}
+	std::cout << "fib_terrible(50) takes far too long" << std::endl;
+	
 	//Iterative fibonacci O(n) time, O(1) space.
-	std::cout << "Iterative fibonacci" << std::endl;
-	//fib(0) = 0
-	begin = clock();
-	std::cout << "fib_i(0) = " << fib_i(0) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
-	//fib(1) = 1
-	begin = clock();
-	std::cout << "fib_i(1) = " << fib_i(1) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
-	//fib(2) = 1
-	begin = clock();
-	std::cout << "fib_i(2) = " << fib_i(2) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
-	//fib(3) = 2
-	begin = clock();
-	std::cout << "fib_i(3) = " << fib_i(3) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
-	//fib(5) = 5
-	begin = clock();
-	std::cout << "fib_i(5) = " << fib_i(5) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
-	//fib(10) = 55
-	begin = clock();
-	std::cout << "fib_i(10) = " << fib_i(10) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
-	//fib(20) = 6765
-	begin = clock();
-	std::cout << "fib_i(20) = " << fib_i(20) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
-	//fib(50) = 12586269025
-	begin = clock();
-	std::cout << "fib_i(50) = " << fib_i(50) << " ";
-	std::cout << double(clock() - begin) / CLOCKS_PER_SEC << std::endl;
+	std::cout << "\nIterative fibonacci" << std::endl;
+	for(int i : test_items){
+		begin = std::chrono::high_resolution_clock::now();
+		std::cout << "fib_i(" << i << ") = " << fib_i(i) << " ";
+		std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - begin).count() << std::endl;
+	}
 	
 	return 0;
 }
